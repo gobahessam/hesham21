10637232e85b055c010eb244582c7e90e562bb0e
gobahessam <65198407+gobahessam@users.noreply.github.com>
2021-01-23 02:08:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// Not interested to writed I Am Rochie everywhere , just enjoy the code 😃
+
+alert ("  هذاء اهداء من عصام الى التافه هشام بمناسبه عيد ميلاده وعقبال 11111111111 سنة ضوئيه  الي يشتي يسمه الاغنية يضغط فوق الصورة" )
+
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
+function play(){ document.getElementById("audio").play();
+}
